95d7b880316a87c9851b86eb3cf0b489e354713e
Rick Conrad <richardjconrad@gmail.com>
2017-08-09 16:32:19 +0100

    Validation tests for WorkHour

--- test/models/vacation_test.rb
+++ test/models/vacation_test.rb
@@ -10,4 +10,6 @@ class VacationTest < ActiveSupport::TestCase
     params = {employee: @luke, start_date: '2017-08-09', end_date: '2017-08-10'}
     model_validation_hack_test Vacation, params
   end
+
+  # TODO Invalid start and end dates, end date before start date, overlaps existing. Start=end date ok
 end
--- test/models/work_hour_test.rb
+++ test/models/work_hour_test.rb
@@ -7,7 +7,30 @@ class WorkHourTest < ActiveSupport::TestCase
   end
 
   test "Validate Presence of Required Attributes" do
-    params = {employee: @luke, date: '2017-08-09', hours: 9}
-    model_validation_hack_test WorkHour, params
+    model_validation_hack_test WorkHour, some_valid_params
+  end
+
+  test "Invalid Date" do
+    params = some_valid_params
+    params[:date] = 'abc'
+    refute WorkHour.new(params).save, "Should not save with invalid date"
+    params[:date] = ''
+    refute WorkHour.new(params).save, "Should not save with blank date"
+  end
+
+  test "Invalid hours" do
+    params = some_valid_params
+    params[:hours] = 'a'
+    refute WorkHour.new(params).save, "Should not save with invalid hours"
+  end
+
+  test "Overlaps Vacation" do
+    params = some_valid_params
+    params[:date] = '2017-07-01'
+    refute WorkHour.new(params).save, "Should not save if date is during vacation"
+  end
+
+  def some_valid_params
+    {employee: @luke, date: '2017-08-09', hours: 9}
   end
 end

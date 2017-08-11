5c52bf6ecf2550be89ac5a86ee50f8e44e5862b1
Brian Yee <signup@yeemail.org>
2017-08-11 15:27:25 +0100

    Basic CRUD for Employees
    Lifecycle works for employees with small set of
    fields (first_name and last_name)

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -21,6 +21,22 @@ class EmployeesController < ApplicationController
     @employee = Employee.find(params[:id])
   end
 
+  def edit
+    @employee = Employee.find(params[:id])
+  end
+
+  def update
+    @employee = Employee.update(params[:id], employee_params)
+    redirect_to employees_path
+  end
+
+  def destroy
+    @employee = Employee.find(params[:id])
+    @employee.destroy
+
+    redirect_to employees_path
+  end
+
   private
     def employee_params
         params.require(:employee).permit(:first_name, :last_name, :name, :title, :department)

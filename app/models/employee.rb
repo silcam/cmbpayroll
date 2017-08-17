0cb1fa74a1e0d96becd58354578a7b08937b9597
Rick Conrad <richardjconrad@gmail.com>
2017-08-17 13:59:28 +0100

    Sort employees alphabetically by default

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -6,6 +6,8 @@ class Employee < ApplicationRecord
 
   validates :first_name, :last_name, presence: {message: I18n.t(:Not_blank)}
 
+  default_scope { order(:last_name, :first_name)}
+
   def full_name
     "#{first_name} #{last_name}"
   end

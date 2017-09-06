1eca4ad70aea4a6816e03b765e470568a4bf7f2c
Rick Conrad <richardjconrad@gmail.com>
2017-09-06 12:20:49 +0100

    Update children to use nested routes and fix validation bug with is_student field

--- app/models/child.rb
+++ app/models/child.rb
@@ -2,5 +2,5 @@ class Child < ApplicationRecord
 
   belongs_to :employee
 
-  validates :first_name, :last_name, :birth_date, :is_student, presence: {message: I18n.t(:Not_blank)}
+  validates :first_name, :last_name, :birth_date, presence: {message: I18n.t(:Not_blank)}
 end
--- test/models/child_test.rb
+++ test/models/child_test.rb
@@ -3,48 +3,32 @@ require "test_helper"
 
 class ChildTest < ActiveSupport::TestCase
 
-#describe Child do
-#  let(:child) { Child.new }
-#
-#  @child.first_nane = 'Test First Name'
-#  child.last_name = 'Test Last Name'
-#  child.birth_date = '2014-01-01'
-#  child.is_student = true
-#  child.employee_id = 8
-#
-#  it "must be valid" do
-#    value(child).must_be :valid?
-#  end
-#end
+  def setup
+    @luke = employees :Luke
+  end
+
 
   test "child is has association" do
     t = Child.reflect_on_association(:employee).macro == :belongs_to
   end
 
   test "child is valid" do
-
-    # reduce redundancy here
-    employee = Employee.new
-
-    employee.first_name = "Bob"
-    employee.last_name = "BobLastName"
-    employee.title = "Assistant Assistant"
-    employee.department = "Dept of Redundancy Dept"
-    employee.hours_day = 18
-
-    employee.save
-
-    assert_not_nil(employee.id, "Employee should have id now")
-
     params = {
         first_name: 'Child',
         last_name: 'Name',
         birth_date: '2008-01-01T01:01:01+01:00',
-        is_student: true,
-        employee_id: employee.id
+        employee_id: @luke.id
     }
 
     model_validation_hack_test Child, params
+  end
+
+  test "Should validate if child is not a student" do
+    assert @luke.children.create!(first_name: 'F',
+                                        last_name: 'L',
+                                        birth_date: Date.new(2017, 7, 4),
+                                        is_student: false)
+    refute @luke.children.last.is_student
   end
 
 end

c05addd962f033fd5fb93d2dbe6a7f3fc500c766
Rick Conrad <richardjconrad@gmail.com>
2017-09-11 10:50:01 +0100

    Unit Tests for Person model

--- test/models/person_test.rb
+++ test/models/person_test.rb
@@ -3,10 +3,45 @@ require "test_helper"
 class PersonTest < ActiveSupport::TestCase
 
   def setup
-
+    @luke = people :Luke
+    @lukejr = people :LukeJr
   end
 
-  test "Playing around" do
+  test "Associations" do
+    assert_equal employees(:Luke), @luke.employee
+    assert_equal users(:Luke), @luke.user
+    assert_equal children(:LukeJr), @lukejr.child
+    assert_includes @luke.children, @lukejr.child
+  end
 
+  test "Presence Validations" do
+    model_validation_hack_test(Person, some_valid_params)
+  end
+
+  test "Valid Gender" do
+    # Accept nil, 0 or 1
+    joe = Person.create!(first_name: 'Joe', last_name: 'Schmoe')
+    joe.gender = :male
+    joe.save!
+    assert_raises (ArgumentError){ joe.gender = :t_rex }
+  end
+
+  test "Full Name" do
+    assert_equal 'Luke Skywalker', @luke.full_name
+  end
+
+  test "Full Name Rev" do
+    assert_equal 'Skywalker, Luke', @luke.full_name_rev
+  end
+
+  test "Non-users list" do
+    nonusers = Person.non_users
+    assert_includes nonusers, people(:Chewie)
+    refute_includes nonusers, @luke
+    refute_includes nonusers, @lukejr
+  end
+
+  def some_valid_params
+    {first_name: 'Ferris', last_name: 'Bueller'}
   end
 end

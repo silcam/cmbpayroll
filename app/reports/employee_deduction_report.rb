4902539836865cd99a3e0883d6433b5d423a56e8
Brian Yee <signup@yeemail.org>
2019-06-19 15:57:43 +0100

    Employee Deduction Report: Various Updates
    * The union column was blank.
    * Some items were not counting correctly. This was due to
      how the sums were computed.
    * Added columns for Bank Transfers, Location Transfers, as
      well as Salary Transfers.
    * Adjusted spacing to handle more columns

--- app/reports/employee_deduction_report.rb
+++ app/reports/employee_deduction_report.rb
@@ -6,58 +6,55 @@ SELECT
   CONCAT(p.last_name, ' ', p.first_name) as Employee_Name,
   e.id,
   ps.gross_pay,
-  SUM(dsa.amount) as Salary_Advance,
+  dsa.amount as Salary_Advance,
+  dbt.amount as Bank_Transfer,
+  dlt.amount as Loc_Transfer,
   ps.total_tax,
-  SUM(dun.amount) as Union,
-  SUM(dlo.amount) as Loan_Payment,
-  SUM(dph.amount) as Photocopies,
-  SUM(dte.amount) as Telephone,
-  SUM(dwa.amount) as Utilities,
-  SUM(dad.amount) as AMICAL,
-  SUM(dot.amount) as Other,
+  ps.union_dues as Union,
+  dlo.amount as Loan_Payment,
+  dph.amount as Photocopies,
+  dte.amount as Telephone,
+  dwa.amount as Utilities,
+  dad.amount as AMICAL,
+  dot.amount as Other,
   ps.net_pay
 FROM
   people p,
   employees e,
   payslips ps
   LEFT OUTER JOIN
-    (SELECT payslip_id, note, amount FROM deductions WHERE note = 'Loan Payment') dlo
+    (SELECT payslip_id, SUM(amount) as amount FROM deductions WHERE note = 'Loan Payment' GROUP BY payslip_id) dlo
     ON dlo.payslip_id = ps.id
   LEFT OUTER JOIN
-    (SELECT payslip_id, note, amount FROM deductions WHERE note = 'AMICAL' OR note = 'amical') dad
+    (SELECT payslip_id, SUM(amount) as amount FROM deductions WHERE note = 'AMICAL' OR note = 'amical' GROUP BY payslip_id) dad
     ON dad.payslip_id = ps.id
   LEFT OUTER JOIN
-    (SELECT payslip_id, note, amount FROM deductions WHERE note = 'Water') dwa
+    (SELECT payslip_id, SUM(amount) as amount FROM deductions WHERE note = 'Water' GROUP BY payslip_id) dwa
     ON dwa.payslip_id = ps.id
   LEFT OUTER JOIN
-    (SELECT payslip_id, note, amount FROM deductions WHERE note = 'Other') dot
+    (SELECT payslip_id, SUM(amount) as amount FROM deductions WHERE note = 'Other' GROUP BY payslip_id) dot
     ON dot.payslip_id = ps.id
   LEFT OUTER JOIN
-    (SELECT payslip_id, note, amount FROM deductions WHERE note = 'Union' OR note = 'union') dun
-    ON dun.payslip_id = ps.id
-  LEFT OUTER JOIN
-    (SELECT payslip_id, note, amount FROM deductions WHERE note = 'Photocopies') dph
+    (SELECT payslip_id, SUM(amount) as amount FROM deductions WHERE note = 'Photocopies' GROUP BY payslip_id) dph
     ON dph.payslip_id = ps.id
   LEFT OUTER JOIN
-    (SELECT payslip_id, note, amount FROM deductions WHERE note = 'Telephone') dte
+    (SELECT payslip_id, SUM(amount) as amount FROM deductions WHERE note = 'Telephone' GROUP BY payslip_id) dte
     ON dte.payslip_id = ps.id
   LEFT OUTER JOIN
-    (SELECT payslip_id, note, amount FROM deductions WHERE deduction_type = #{Charge.charge_types["advance"]} OR deduction_type = #{Charge.charge_types["location_transfer"]}) dsa
+    (SELECT payslip_id, SUM(amount) as amount FROM deductions WHERE deduction_type = #{Charge.charge_types["advance"]} GROUP BY payslip_id) dsa
     ON dsa.payslip_id = ps.id
+  LEFT OUTER JOIN
+    (SELECT payslip_id, SUM(amount) as amount FROM deductions WHERE deduction_type = #{Charge.charge_types["bank_transfer"]} GROUP BY payslip_id) dbt
+    ON dbt.payslip_id = ps.id
+  LEFT OUTER JOIN
+    (SELECT payslip_id, SUM(amount) as amount FROM deductions WHERE deduction_type = #{Charge.charge_types["location_transfer"]} GROUP BY payslip_id) dlt
+    ON dlt.payslip_id = ps.id
 WHERE
   e.employment_status IN :employment_status AND
   e.person_id = p.id AND
   e.id = ps.employee_id AND
   ps.period_year = :year AND
   ps.period_month = :month
-GROUP BY
-  ps.id,
-  p.first_name,
-  p.last_name,
-  e.id,
-  ps.gross_pay,
-  ps.total_tax,
-  ps.net_pay
 ORDER BY
   p.last_name ASC;
     SELECTSTATEMENT

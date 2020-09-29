6edc17d0e6063da96d771652573eac617466a63c
Brian Yee <brian.yee@sil.org>
2020-09-29 12:53:10 +0100

    Fix test

--- test/reports/cmb_report_test.rb
+++ test/reports/cmb_report_test.rb
@@ -2,14 +2,11 @@ require "test_helper"
 
 class CmbReportTests < ActiveSupport::TestCase
 
-  test "All Statuses" do
+  test "Active Statuses" do
     exp = []
     exp.push(Employee.employment_statuses[:full_time])
     exp.push(Employee.employment_statuses[:part_time])
     exp.push(Employee.employment_statuses[:temporary])
-    exp.push(Employee.employment_statuses[:leave])
-    exp.push(Employee.employment_statuses[:terminated_to_year_end])
-    exp.push(Employee.employment_statuses[:inactive])
 
     report = DipesReport.new(period: LastPostedPeriod.current.to_s)
     ary = report.employment_status()

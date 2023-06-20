cdb3db477e63a1d4f3194802d63e7e393ed9655b
Brian Yee <brian.yee@sil.org>
2023-06-20 11:40:21 -0400

    updates to ruby version to support newer gems

--- Gemfile
+++ Gemfile
@@ -22,6 +22,11 @@ gem 'uglifier', '>= 1.3.0'
 # Use jquery
 gem 'jquery-rails', '~>4.3.1'
 
+
+# See: https://github.com/rails/rails/issues/34822
+# And https://github.com/ruby/bigdecimal#which-version-should-you-select
+gem 'bigdecimal', '1.3.5'
+
 # Use CoffeeScript for .coffee assets and views
 gem 'coffee-rails', '~> 4.2'
 # Turbolinks makes navigating your web application faster. Read more: https://github.com/turbolinks/turbolinks
---
+++ config/initializers/big_decimal_suppressor.rb
@@ -0,0 +1,18 @@
+# Problem on Ruby 2.7
+# > BigDecimal(1).to_s("F")
+# (pry):1: warning: rb_check_safe_obj will be removed in Ruby 3.0
+# This monkeypatch silences it.
+# TODO: remove when on Ruby 3+
+module BigDecimalFormatWarningSuppressor
+  # NOTE: this API comes from ActiveSupport::NumericWithFormat, the last ancestor prepended to
+  # BigDecimal.
+  def to_s(format = nil, options = nil)
+    original_verbosity = $VERBOSE
+    $VERBOSE = nil
+    v = super
+    $VERBOSE = original_verbosity
+    v
+  end
+end
+
+BigDecimal.prepend(BigDecimalFormatWarningSuppressor)
